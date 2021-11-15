616e4e4ae0caefa1b935a7c791f8a73faeb17b21
d0phamine <sizow123@gmail.com>
2021-11-15 09:11:13 +0300

    123

--- sa-logic/sa/sentiment_analysis.py
+++ sa-logic/sa/sentiment_analysis.py
@@ -1,18 +1,37 @@
 from textblob import TextBlob
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, json
+from PIL import Image, ExifTags
+import requests
 
 app = Flask(__name__)
 
 
-@app.route("/analyse/sentiment", methods=['POST'])
-def analyse_sentiment():
-    sentence = request.get_json()['sentence']
-    polarity = TextBlob(sentence).sentences[0].polarity
-    return jsonify(
-        sentence=sentence,
-        polarity=polarity
-    )
+@app.route("/exif", methods=['POST'])
+def exifImage():
+    print("exif image")
+    print(request.get_json())
+    a = request.get_json()
+    print(type(a))
+    url="http://127.0.0.1:4000/" + a['body']['path']
+
+    req = requests.get(url, stream = True)
+
+    # img = request.files.get('file', '')
+
+    with open ("image.png", "wb") as f:
+        for chunk in req:
+            f.write(chunk)
+    img = Image.open("image.png")
+    print("111")
+    exifData = {
+        ExifTags.TAGS[k]: v
+        for k, v in img._getexif().items()
+        if k in ExifTags.TAGS
+    }
+
+    print(exifData)
+    return str(exifData)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(port=5000)
