265f0057b54e4692ed6033cf3309ac160e2d1078
d0phamine <sizow123@gmail.com>
2021-11-29 17:30:47 +0300

    +

--- sa-logic/sa/sentiment_analysis.py
+++ sa-logic/sa/sentiment_analysis.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 @app.route("/exif", methods=['POST'])
 def exifImage():
     print("exif image")
-    print(request.get_json())
+    # print(request.get_json())
     a = request.get_json()
-    print(type(a))
-    print(a)
+    # print(type(a))
+    # print(a)
     # url="http://127.0.0.1:3333/" + a['body']['file']['tmpPath']
 
     # req = requests.get(url, stream = True)
@@ -22,7 +22,14 @@ def exifImage():
     # with open ("image.png", "wb") as f:
     #     for chunk in req:
     #         f.write(chunk)
-    img = Image.open(a['body'])
+    req = requests.get("http://127.0.0.1:3333/uploads/"+a["fileName"], stream = True)
+    if req.status_code == 200:
+        with open("image.png", "wb") as f:
+            for chunk in req:
+                f.write(chunk)
+    # f = open("qwer.png", "w")
+    # f.write(req.content)
+    img = Image.open("image.png")
     print(img)
     try:
         exifData = {
